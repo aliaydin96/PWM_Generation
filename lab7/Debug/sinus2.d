e91a6af768af69773c796425cf9a393cd3261e89
Ali Aydin <ali.aydin@metu.edu.tr>
2018-08-07 14:02:48 +0300

    I generated three phase shifted sinus by using floating property of DSP

---
+++ lab7/Debug/sinus2.d
@@ -0,0 +1,49 @@
+# FIXED
+
+sinus2.obj: ../sinus2.c
+sinus2.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_Device.h
+sinus2.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_Adc.h
+sinus2.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_DevEmu.h
+sinus2.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_CpuTimers.h
+sinus2.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_ECan.h
+sinus2.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_ECap.h
+sinus2.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_DMA.h
+sinus2.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_EPwm.h
+sinus2.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_EQep.h
+sinus2.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_Gpio.h
+sinus2.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_I2c.h
+sinus2.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_McBSP.h
+sinus2.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_PieCtrl.h
+sinus2.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_PieVect.h
+sinus2.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_Spi.h
+sinus2.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_Sci.h
+sinus2.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_SysCtrl.h
+sinus2.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_XIntrupt.h
+sinus2.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_Xintf.h
+sinus2.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-c2000_16.9.7.LTS/include/math.h
+sinus2.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-c2000_16.9.7.LTS/include/linkage.h
+sinus2.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-c2000_16.9.7.LTS/include/_defs.h
+
+../sinus2.c: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_Device.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_Adc.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_DevEmu.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_CpuTimers.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_ECan.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_ECap.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_DMA.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_EPwm.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_EQep.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_Gpio.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_I2c.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_McBSP.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_PieCtrl.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_PieVect.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_Spi.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_Sci.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_SysCtrl.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_XIntrupt.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_Xintf.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-c2000_16.9.7.LTS/include/math.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-c2000_16.9.7.LTS/include/linkage.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-c2000_16.9.7.LTS/include/_defs.h: 
